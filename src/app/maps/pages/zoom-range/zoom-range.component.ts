64babcb22977583e270d579a51e359031864397f
Carlos Verdugo <carlos.versa99@gmail.com>
2024-10-06 10:55:11 -0700

    Adding structure for maps & routing

---
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './zoom-range.component.html',
+  styleUrl: './zoom-range.component.css'
+})
+export class ZoomRangeComponent {
+
+}
